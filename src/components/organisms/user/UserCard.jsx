cf6d26faa17d2f77fca77c2de8f40a6c2304fede
nijimajohn <86758487+nijimajohn@users.noreply.github.com>
2021-12-11 11:36:40 +0900

    Initial commit

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>1111@aaa.com</dd>
+        <dt>TEL</dt>
+        <dd>000-000-000</dd>
+        <dt>会社名</dt>
+        <dd>じゃけぇ会社</dd>
+        <dt>Web</dt>
+        <dd>jake.com</dd>
+      </dl>
+    </div>
+  );
+};
